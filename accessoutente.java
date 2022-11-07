8fc99c56618fdb656c957d2eb32d107eb4b07158
nandojv <116838266+nandojv@users.noreply.github.com>
2022-11-07 23:19:10 +0100

    Add files via upload

---
+++ accessoutente.java
@@ -0,0 +1,57 @@
+package corsoiniziodacapo;
+
+import java.util.Scanner;
+
+public class accessoutente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				System.out.println("Nome");
+				
+				String nome = scanner.nextLine();
+				System.out.println(nome);
+				
+				//concatenazione di stringhe
+				
+				System.out.println(" Cognome");
+				scanner.nextLine();
+				
+				
+				System.out.println("Eta");
+			    scanner.nextLine();
+				
+				System.out.println("Citta");
+				scanner.nextLine();
+
+				
+				System.out.println("Email");
+				scanner.nextLine();
+				
+				System.out.println("benvenuto");
+				scanner.nextLine();
+
+				
+				
+				
+			}
+
+
+
+
+
+
+
+
+
+
+					
+				
+
+			
+
+	}
+
+}
